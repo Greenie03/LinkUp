28f29ed0b548a7309ae13e11534b1a92ca3eb1d7
Greenie03 <otyazoula@gmail.com>
2026-07-24 10:37:14 +0200

    Env Variables Load

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,16 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { Neo4jModule } from './neo4j/neo4j.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [Neo4jModule.forRoot({
-    scheme: 'neo4j+s',
-    host: "",
-    port: "",
-    username: "",
-    password: "",
-    database: "",
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    Neo4jModule.forRoot({
+    scheme: process.env.NEO4J_SCHEME || 'neo4j',
+    host: process.env.NEO4J_HOST || "",
+    port: process.env.NEO4J_PORT || "",
+    username: process.env.NEO4J_USERNAME || "",
+    password: process.env.NEO4J_PASSWORD || "",
+    database: process.env.NEO4J_DATABASE || "",
 
   })],
   controllers: [AppController],
